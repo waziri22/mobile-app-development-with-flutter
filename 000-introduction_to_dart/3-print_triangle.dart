d4e7f7e40a28780085c77d5ce4209b8d3bdcf2c1
waziri22 <120272280+waziri22@users.noreply.github.com>
2023-11-27 02:06:02 -0800

    3-print_triangle.dart

--- 000-introduction_to_dart/3-print_triangle.dart
+++ 000-introduction_to_dart/3-print_triangle.dart
@@ -1,33 +1,15 @@
 void printRightAngleTriangle(int height)
-
-  
 {
-
-  
   String triangleChar = '*';   
-
-  
   print('');
-  
-
   int j = 0;
-  
-
   while (j <= height)
-  
-  
   {
     print(triangleChar * j);
-
-    
     j += 1;
   }
 }
-
-
 void main() 
-
-
 {
   printRightAngleTriangle(5);
 }
