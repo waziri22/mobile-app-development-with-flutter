135cd658137d8f2e8c9664c6aedc8972bec1b7fb
waziri22 <120272280+waziri22@users.noreply.github.com>
2023-11-27 01:44:19 -0800

    1-smallest_int.dart

--- 000-introduction_to_dart/1-smallest_int.dart
+++ 000-introduction_to_dart/1-smallest_int.dart
@@ -1,15 +1,33 @@
 import 'dart:math';
 
-class GetSmallestInteger {
-  void getSmallestInteger(List<int> myList) {
+
+class GetSmallestInteger 
+{
+
+  
+  void getSmallestInteger(List<int> myList)
+
+
+  {
     print("Smallest element is: ${myList.reduce(min)}");
   }
+
+  
 }
 
-void main() {
+void main()
+
+
+{
+
+  
   var myList = [10, 20, 1, 45, 99];
+  
 
   var p = GetSmallestInteger();
+  
 
   p.getSmallestInteger(myList);
+
+  
 }
