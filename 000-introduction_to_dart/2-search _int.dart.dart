9c0b27ef2a0ed16318d60b836c88ffcdbb8b42b4
waziri22 <120272280+waziri22@users.noreply.github.com>
2023-11-27 01:45:41 -0800

    2-search _int.dart.dart

--- 000-introduction_to_dart/2-search _int.dart.dart
+++ 000-introduction_to_dart/2-search _int.dart.dart
@@ -1,12 +1,32 @@
-class FindIndexOfFirstOccurrence {
-  FindIndexOfFirstOccurrence(List<int> myList, int target) {
+class FindIndexOfFirstOccurrence 
+
+
+{
+  FindIndexOfFirstOccurrence(List<int> myList, int target)
+  
+  
+  {
     int index = myList.indexOf(target);
+
+    
      print (index);
   }
+
+  
 }
 
-void main() {
+
+void main()
+
+
+{
   var myList = [1, 3, 4, 2];
+
+  
   var target = 1;
+
+  
   var w = FindIndexOfFirstOccurrence(myList, target);
+
+  
 }
