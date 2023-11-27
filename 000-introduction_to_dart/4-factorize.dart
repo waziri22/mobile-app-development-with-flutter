ce761e6ac67f561bdad55ba2dd47fcc1ef5a0b24
waziri22 <120272280+waziri22@users.noreply.github.com>
2023-11-27 02:05:10 -0800

    4-factorize.dart

--- 000-introduction_to_dart/4-factorize.dart
+++ 000-introduction_to_dart/4-factorize.dart
@@ -1,55 +1,29 @@
 import 'dart:math';
 
-
 import 'dart:io';
-
-
 void factorizeToPrimeNumbers(int number)
-
- 
  {
   while (number % 2 == 0) 
-  
-  
   {
     print(2);
-
-   
     number = number ~/ 2;
   }
-
-
   for (int i = 3; i <= sqrt(number); i += 2)
-  
-  
   {
     while (number % i == 0) 
-    
-    
     {
       print(i);
-
-     
       number = number ~/ i;
     }
   }
-
   if (number > 2) {
     print(number);
   }
 }
-
-
 void main() 
-
-
 {
  print('Enter number');
-
- 
   int number = int.parse(stdin.readLineSynch());
-
- 
   factorizeToPrimeNumbers(number);
 }
 
