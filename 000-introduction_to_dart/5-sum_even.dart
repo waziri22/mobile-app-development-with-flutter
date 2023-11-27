0403d8abac3be21e39a1af499930e9d2abcea6ad
waziri22 <120272280+waziri22@users.noreply.github.com>
2023-11-27 01:50:57 -0800

    5-sum_even.dart

--- 000-introduction_to_dart/5-sum_even.dart
+++ 000-introduction_to_dart/5-sum_even.dart
@@ -1,21 +1,49 @@
 import 'dart:io';
 
-  int sumOfEvenNumbers(List<int> numbers) {
-    int sum = 0;
 
-    numbers.forEach((numbers) {
-      if (numbers % 2 == 0) {
+  int sumOfEvenNumbers(List<int> numbers)
+  
+  
+  {
+    int sum = 0;
+    
+
+    numbers.forEach((numbers)
+                    
+    
+    {
+      if (numbers % 2 == 0) 
+      
+      
+      {
         sum += numbers;
+
+        
        numbers++;
-      } else {
+
+        
+      } else
+      
+      
+      {
         print('$numbers is not even');
       }
+
+      
     });
       return sum;
   }
 
-void main() {
+
+void main()
+
+
+{
   print('Enter numbers :');
+
+  
   int numbers = int.parse(stdin.readLineSynch());
+
+  
   sumOfEvenNumbers(numbers);
 }
