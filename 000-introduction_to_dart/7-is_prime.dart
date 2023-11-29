b8a2d3d202e8eb475dc5ef5faa9ccb121a8bf9a4
waziri22 <120272280+waziri22@users.noreply.github.com>
2023-11-29 21:29:46 +0300

    7-is_prime.dart

--- 000-introduction_to_dart/7-is_prime.dart
+++ 000-introduction_to_dart/7-is_prime.dart
@@ -1,12 +1,12 @@
-import 'dart:io';
+import  'dart:io';
 
-bool isPrime(int number) {
-  if (number <= 1) {
-    return false;
+bool  isPrime(int number) {
+  if  (number <= 1) {
+    return  false;
   }
 
-  for (int i = 2; i <= number / 2; i++) {
-    if (number % i == 0) {
+  for  (int i = 2; i <= number / 2; i++) {
+    if  (number % i == 0) {
       return false;
     }
   }
@@ -14,12 +14,12 @@ bool isPrime(int number) {
   return true;
 }
 
-void main() {
-  print('Enter number');
-  var number = int.parse(stdin.readLineSync()!);
+void  main () {
+  print  ('Enter number');
+  var number = int.parse  (stdin.readLineSync()!);
   if (isPrime(number)) {
-    print('$number is a prime number.');
+    print  ('$number is a prime number.');
   } else {
-    print('$number is not a prime number.');
+    print  ('$number is not a prime number.');
   }
 }
