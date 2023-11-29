94c32b4777862e10fad832f54dc36c530c5cfd14
waziri22 <120272280+waziri22@users.noreply.github.com>
2023-11-29 21:27:19 +0300

    6-char_frequency.dart

--- 000-introduction_to_dart/6-char_frequency.dart
+++ 000-introduction_to_dart/6-char_frequency.dart
@@ -1,29 +1,29 @@
 import 'dart:io';
 
-void printFrequency(String str)
+void  printFrequency (String str)
 {
   Map<String, int> frequencyMap = {};
   String word = "";
   for (int i = 0; i < str.length; i++)
   {
-    if (str[i] == ' ') 
+    if  (str[i] == ' ') 
     {
-      if (word.isNotEmpty) 
+      if  (word.isNotEmpty) 
       {
         frequencyMap[word] = (frequencyMap[word] ?? 0) + 1;
         word = "";
       }
     } else 
     {
-      word += str[i];
+      word += str [i];
     }
   }
   
-  if (word.isNotEmpty) 
+  if  (word.isNotEmpty) 
   {
     frequencyMap[word] = (frequencyMap[word] ?? 0) + 1;
   }
-  for (var entry in frequencyMap.entries)
+  for  (var entry in frequencyMap.entries)
   {
     print('${entry.key} - ${entry.value}');
   }
