7ffdbb39cc6390b972a8ab2b470e5eb8b405b459
waziri22 <120272280+waziri22@users.noreply.github.com>
2024-02-05 05:14:26 -0800

    main.dart

--- loCation/main.dart
+++ loCation/main.dart
@@ -1,69 +1,125 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class PictureUploader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Upload Picture'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            _uploadPicture(context);
+          },
+          child: Text('Upload Picture'),
+        ),
+      ),
     );
+  }
+
+  Future<void> _uploadPicture(BuildContext context) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      final File file = File(pickedFile.path);
+
+      // Prepare the request
+      var uri = Uri.parse('http://localhost/project/location/uploadphoto.php');
+      var request = http.MultipartRequest('POST', uri);
+      request.files.add(
+        http.MultipartFile(
+          'image',
+          file.readAsBytes().asStream(),
+          file.lengthSync(),
+          filename: file.path.split('/').last,
+        ),
+      );
+
+      // Send the request
+      var response = await request.send();
+
+      if (response.statusCode == 200) {
+        // Picture uploaded successfully
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Picture uploaded successfully'),
+        ));
+      } else {
+        // Error uploading picture
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Error uploading picture'),
+        ));
+      }
+    }
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
+class PictureViewer extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _PictureViewerState createState() => _PictureViewerState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _PictureViewerState extends State<PictureViewer> {
+  List<PictureInfo> pictures = [];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _fetchPictures();
+  }
+
+  Future<void> _fetchPictures() async {
+    final response = await http.get(Uri.parse('http://localhost/project/location/get.php'));
+
+    if (response.statusCode == 200) {
+      Iterable list = json.decode(response.body);
+      setState(() {
+        pictures = list.map((model) => PictureInfo.fromJson(model)).toList();
+      });
+    } else {
+      // Handle error
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('Pictures'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView.builder(
+        itemCount: pictures.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(pictures[index].path),
+            subtitle: Text(
+              'Latitude: ${pictures[index].latitude}, Longitude: ${pictures[index].longitude}',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+            leading: Image.network('http://localhost/${pictures[index].path}'),
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
+  }
+}
+
+class PictureInfo {
+  final String path;
+  final double latitude;
+  final double longitude;
+
+  PictureInfo({required this.path, required this.latitude, required this.longitude});
+
+  factory PictureInfo.fromJson(Map<String, dynamic> json) {
+    return PictureInfo(
+      path: json['path'],
+      latitude: double.parse(json['latitude']),
+      longitude: double.parse(json['longitude']),
     );
   }
 }
